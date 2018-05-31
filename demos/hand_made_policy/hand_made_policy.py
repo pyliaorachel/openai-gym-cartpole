3651503e7496c7beb166da2db51ec0a3768ca765
Peiyu Liao <u3512485@connect.hku.hk>
2018-06-01 00:57:21 +0800

    Finish random aciton, hand-made policy, Q table

---
+++ demos/hand_made_policy/hand_made_policy.py
@@ -0,0 +1,35 @@
+"""
+Agent taking reasonable actions based on hand-made rules,
+i.e. go to right if leaning towards left, vice versa.
+
+Observation space: [position of cart, velocity of cart, angle of pole, rotation rate of pole]
+Action space: [moving cart to left, moving cart to right]
+
+Note that good agents learn the policies by themselves,
+and don't need to know the meaning of the observations.
+"""
+import gym
+
+
+def choose_action(observation):
+    pos, v, ang, rot = observation
+    return 0 if ang < 0 else 1 # a simple rule based only on angles
+
+if __name__ == '__main__':
+    env = gym.make('CartPole-v0')
+
+    for i_episode in range(20):
+        observation = env.reset() # reset environment to initial state for each episode
+        rewards = 0 # accumulate rewards for each episode
+        for t in range(100):
+            env.render()
+
+            action = choose_action(observation) # choose an action based on hand-made rule 
+            observation, reward, done, info = env.step(action) # do the action, get the reward
+            rewards += reward
+
+            if done:
+                print('Episode finished after {} timesteps, total rewards {}'.format(t+1, rewards))
+                break
+
+    env.close() # need to close, or errors will be reported
---
+++ demos/random_action/random_action.py
@@ -0,0 +1,25 @@
+"""
+Agent taking random actions.
+Based on the example on the official website.
+"""
+import gym
+
+
+if __name__ == '__main__':
+    env = gym.make('CartPole-v0')
+
+    for i_episode in range(20):
+        observation = env.reset() # reset environment to initial state for each episode
+        rewards = 0 # accumulate rewards for each episode
+        for t in range(100):
+            env.render()
+
+            action = env.action_space.sample() # choose a random action
+            observation, reward, done, info = env.step(action) # do the action, get the reward
+            rewards += reward
+
+            if done:
+                print('Episode finished after {} timesteps, total rewards {}'.format(t+1, rewards))
+                break
+
+    env.close() # need to close, or errors will be reported
